85274472f0e2f89454a9bc1f35d1b12f1c740343
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2022-09-17 10:05:45 +0200

    adding colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        almostWhite: "hsl(0, 0%, 98%)",
+        mediumGray: "hsl(0, 0%, 41%)",
+        almostBlack: "hsl(0, 0%, 8%)",
+      },
+    },
   },
   plugins: [],
-}
+};
